953e6a7c5aeafdac95d07716ffa61320130b5089
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-10 10:25:50 +0100

    msm8226.mk: drop audio.a2dp.default

--- msm8226.mk
+++ msm8226.mk
@@ -49,7 +49,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.primary.msm8226 \
     audio.r_submix.default \
     audio.usb.default \
